90a99ed646aea0f86394e0d104919d0a9717f1ec
pdfosborne <pdfosborne@gmail.com>
2025-01-14 13:53:47 +0000

    Moved language data into adapter

--- adapters/classroom_A_language.py
+++ adapters/classroom_A_language.py
@@ -7,13 +7,36 @@ from torch import Tensor
 from elsciRL.encoders.sentence_transformer_MiniLM_L6v2 import LanguageEncoder
 
 # Language data 
-from adapters.language_data.student_features import data as StudentFeatures
+#from adapters.language_data.student_features import data as StudentFeatures
 
 class LanguageAdapter:
     _cached_state_idx: Dict[str, int] = dict()
 
     def __init__(self, setup_info:dict={}) -> None:
-        self.student_features = StudentFeatures
+        self.student_features = {
+            '4_1': {'type': 'student','hair_colour': 'black','hair_style': 'medium curly','upperclothing_type': 'hoodie','upperclothing_colour': 'black',
+                    'lowerclothing_type': 'denim jeans','lowerclothing_colour': 'black','piercings': 'ear', 'gender': 'female'},
+            '3_1': {'type': 'student','hair_colour': 'black','hair_style': 'long straight','upperclothing_type': 'hoodie','upperclothing_colour': 'black',
+                    'lowerclothing_type': 'denim jeans','lowerclothing_colour': 'black','piercings': 'ear', 'gender':'male'},
+            '2_1': {'type': 'student','hair_colour': 'black','hair_style': 'long medium','upperclothing_type': 'hoodie','upperclothing_colour': 'black',
+                    'lowerclothing_type': 'denim jeans','lowerclothing_colour': 'black','piercings': 'face','gender': 'female'},
+            '1_1': {'type': 'student','hair_colour': 'brown','hair_style': 'medium straight','upperclothing_type': 'jacket','upperclothing_colour': 'blue',
+                    'lowerclothing_type': 'denim jeans','lowerclothing_colour': 'black','piercings': 'no','gender': 'male'},
+            '1_2': {'type': 'student','hair_colour': 'blonde','hair_style': 'long straight','upperclothing_type': 'hoodie','upperclothing_colour': 'red',
+                    'lowerclothing_type': 'denim jeans','lowerclothing_colour': 'black','piercings': 'ear','gender': 'female'},
+            '1_3': {'type': 'student','hair_colour': 'brown','hair_style': 'short','upperclothing_type': 'shirt','upperclothing_colour': 'blue',
+                    'lowerclothing_type': 'chinos','lowerclothing_colour': 'brown','piercings': 'no','gender': 'male'},
+            '2_3': {'type': 'student','hair_colour': 'blonde','hair_style': 'long straight','upperclothing_type': 'cardigan','upperclothing_colour': 'blue',
+                    'lowerclothing_type': 'chinos','lowerclothing_colour': 'brown','piercings': 'no','gender':'female'},
+            '3_3': {'type': 'teacher','hair_colour': 'brown','hair_style': 'short','upperclothing_type': 'jacket','upperclothing_colour': 'brown',
+                    'lowerclothing_type': 'chinos','lowerclothing_colour': 'blue','piercings': 'no', 'gender': 'male'},
+            '3_2': {'type': 'student','hair_colour': 'pink','hair_style': 'mohawk','upperclothing_type': 'studded denim jacket','upperclothing_colour': 'blue',
+                    'lowerclothing_type': 'denim jeans','lowerclothing_colour': 'black','piercings': 'face', 'gender': 'male'},
+            '4_3': {'type': 'recycling','hair_colour': 'NA','hair_style': 'NA','upperclothing_type': 'NA','upperclothing_colour': 'NA',
+                    'lowerclothing_type': 'NA','lowerclothing_colour': 'NA','piercings':'NA','gender':'NA'},
+            '4_2': {'type': 'trash','hair_colour': 'NA','hair_style': 'NA','upperclothing_type': 'NA','upperclothing_colour': 'NA',
+                    'lowerclothing_type': 'NA','lowerclothing_colour': 'NA','piercings':'NA','gender':'NA'},
+        }
         self.encoder = LanguageEncoder()
     
     def adapter(self, state:str, legal_moves:list = None, episode_action_history:list = None, encode:bool = True, indexed: bool = False) -> Tensor:
